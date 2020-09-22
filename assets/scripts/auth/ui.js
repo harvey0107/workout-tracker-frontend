d8f6cb8b68480b8a287f36ad222c046455c9a920
rachel yang <rachel@rachels-MacBook-Air.local>
2020-09-22 14:59:06 -0400

    9/22 commit

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -5,13 +5,13 @@ const onSignUpSuccess = function (response) {
   $('#message').text('Welcom my friden' + response.user.email)
   $('#sign-up-form').trigger('reset')
   $('#sign-up-form').hide()
-  $('#message').hide(12000)
+  $('#message').hide(6000)
 }
 const onSignUpFailure = function () {
   $('#message').show()
   $('#message').text('Failed!')
   $('#sign-up-form').trigger('reset')
-  $('#message').hide(12000)
+  $('#message').hide(6000)
 }
 
 const onSignInSuccess = function (response) {
@@ -24,7 +24,7 @@ const onSignInSuccess = function (response) {
   $('#start-game').show()
   $('#sign-in-form').hide()
   $('#sign-up-form').hide()
-  $('#message').hide(12000)
+  $('#message').hide(6000)
   $('#tracker').show()
   $('#update').show()
   $('#record').show()
@@ -35,19 +35,20 @@ const onSignInFailure = function () {
   $('#message').show()
   $('#message').text('Oops, Try again!')
   $('#sign-in-form').trigger('reset')
+  $('#message').hide(6000)
 }
 
 const onChangePasswordSuccess = function () {
   $('#message').show()
   $('#message').text('Changed password successfully')
   $('#change-password-form').trigger('reset')
-  $('#message').hide(12000)
+  $('#message').hide(6000)
 }
 const onChangePasswordFailure = function () {
   $('#message').show()
   $('#message').text('Error')
   $('#change-password-form').trigger('reset')
-  $('#message').hide(12000)
+  $('#message').hide(6000)
 }
 
 const onSignOutSuccess = function () {
@@ -62,11 +63,12 @@ const onSignOutSuccess = function () {
   $('#record').hide()
   $('#remove').hide()
   $('#message').text('See you later')
-  $('#message').hide(12000)
+  $('#message').hide(6000)
 }
 const onSignOutFailure = function () {
   $('#message').text('Oops')
   $('#sign-out-form').trigger('reset')
+  $('#message').hide(6000)
 }
 module.exports = {
   onSignUpSuccess,
